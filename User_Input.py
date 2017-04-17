21b88bdd61f65f945eca67eeb2fe73a0894c2062
楊喻翔 <seanyang@yangyuxiangs-MacBook-Pro.local>
2017-04-17 11:27:20 +0800

    [v1.0] First release Fabric Parser for Android.

---
+++ User_Input.py
@@ -0,0 +1,18 @@
+# Top build version
+Version = '(61000004)'
+
+# GMail label (User should create a label which have policy "send by Fabric" and separate platform iOS/Android as well.)
+Gmail_fabric_label = 'Label_1'
+
+# Issue default assign
+Default_status = 'Open'
+Default_owner = 'Fate'
+
+# iOS Fabric sheet
+# spreadsheet_id = '1ex2ovtXCVkZWuyqZi7awUYYxpK-uxioS-rOdyI6N_8E'
+# Use spreadsheet_id to get "Template" id - https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get
+# sheet_id_template = '1898869629'
+
+# Test sheet
+spreadsheet_id = '1Gx_2izYogh-0PgEej-EtGJrYzUaL_Ci5N4OH0bQLblc'
+sheet_id_template = '1403088925'
